cd8f34634afb9baea67ec364a93d70f3c7ec45f0
MH2033 <aakbarimh@gmail.com>
2024-01-26 21:29:46 +0100

    Launch the relevent test based on cmd args

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[]) {
   int problem_id = 0;
   double deltat = 0.0;
-  std::string mesh_file;
+  std::string mesh_file_name;
   std::string err_msg =
       "Required Arguments: \n"
       "-p problem_id(1: Cylinder2D, 2: Cylinder3D, 3: "
@@ -13,10 +13,6 @@ int main(int argc, char* argv[]) {
       "-t deltat \n"
       "-m path_to_mesh_file";
 
-  if (argc < 4) {
-    std::cerr << err_msg << std::endl;
-    return 1;
-  }
   const char* const short_opts = "p:t:m:";
   const option long_opts[] = {{"problem", required_argument, nullptr, 'p'},
                               {"deltat", required_argument, nullptr, 't'},
@@ -34,7 +30,7 @@ int main(int argc, char* argv[]) {
         deltat = std::stod(optarg);
         break;
       case 'm':
-        mesh_file = optarg;
+        mesh_file_name = optarg;
         break;
       default:
         std::cerr << err_msg << std::endl;
@@ -48,25 +44,41 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  // Print the parsed arguments
-  std::cout << "arg2: " << problem_id << std::endl;
-  std::cout << "arg3: " << deltat << std::endl;
-  std::cout << "arg4: " << mesh_file << std::endl;
-
   Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
 
-  const std::string mesh_file_name = "../mesh/cube-0.25.msh";
   constexpr unsigned int degree_velocity = 2;
   constexpr unsigned int degree_pressure = 1;
   constexpr double T = 1e-2;
-  // constexpr double deltat = 1e-4;
   constexpr double nu = 0.01;
 
-  EthierSteinman problem(mesh_file_name, degree_velocity, degree_pressure, T,
-                         deltat, nu);
+  if (problem_id == 1) {
+    Cylinder2D problem(mesh_file_name, degree_velocity, degree_pressure, T,
+                       deltat, nu);
 
-  problem.setup();
-  problem.solve();
+    problem.setup();
+    problem.solve();
+  } else if (problem_id == 2) {
+    Cylinder3D problem(mesh_file_name, degree_velocity, degree_pressure, T,
+                       deltat, nu);
+
+    problem.setup();
+    problem.solve();
+  } else if (problem_id == 3) {
+    EthierSteinman problem(mesh_file_name, degree_velocity, degree_pressure, T,
+                           deltat, nu);
+
+    problem.setup();
+    problem.solve();
+  } else if (problem_id == 4) {
+    Step problem(mesh_file_name, degree_velocity, degree_pressure, T, deltat,
+                 nu);
+
+    problem.setup();
+    problem.solve();
+  } else {
+    std::cerr << "Error: Problem ID must be 1, 2, 3 or 4." << std::endl;
+    return 1;
+  }
 
   return 0;
 }
