e0db3646f905fec905c859c0b4c2903b46e225a6
Luigi Pagani <127208537+LuigiPagani@users.noreply.github.com>
2024-01-31 12:31:57 +0100

    final commit

--- FK_solver/src/main.cpp
+++ FK_solver/src/main.cpp
@@ -17,55 +17,7 @@ main(int argc, char *argv[])
    problem.solve();
 
 
-  // const unsigned int               mpi_rank =
-  // Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
 
-
-
-  //     ConvergenceTable table;
-
-  // const std::vector<std::string> meshes = {"../mesh/mesh-square-h0.100000.msh",
-  //                                          "../mesh/mesh-square-h0.050000.msh",
-  //                                          "../mesh/mesh-square-h0.025000.msh",
-  //                                          "../mesh/mesh-square-h0.012500.msh"};
-  // const std::vector<double>      h_vals = {0.1,
-  //                                          0.05,
-  //                                          0.025,
-  //                                          0.0125};
-
-  // std::ofstream convergence_file("convergence.csv");
-  // convergence_file << "h,eL2,eH1" << std::endl;
-
-  // T = 0.00000001;
-  // deltat = 0.000000001;
-
-  // for (unsigned int i = 0; i < meshes.size(); ++i)
-  //   {
-  
-  // FK_solver problem(meshes[i], degree, T, deltat);
-
-  //     problem.setup();
-  //     problem.solve();
-
-  //     const double error_L2 = problem.compute_error(VectorTools::L2_norm);
-  //     const double error_H1 = problem.compute_error(VectorTools::H1_norm);
-
-  //     table.add_value("h", h_vals[i]);
-  //     table.add_value("L2", error_L2);
-  //     table.add_value("H1", error_H1);
-
-  //     convergence_file << h_vals[i] << "," << error_L2 << "," << error_H1
-  //                      << std::endl;
-  //   }
-  
-
-  //  if (mpi_rank == 0)
-  //   {
-  //       table.evaluate_all_convergence_rates(ConvergenceTable::reduction_rate_log2);
-  //       table.set_scientific("L2", true);
-  //       table.set_scientific("H1", true);
-  //       table.write_text(std::cout);
-  //   }
 
   return 0;
 }
